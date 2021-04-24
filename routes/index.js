dab69a9753be7d09fcbad7aaa3b395cb3fef6cac
Steve Gekas <75593974+SteveGekas@users.noreply.github.com>
2021-04-24 19:22:44 -0400

    Delete index.js

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const apiRoutes = require("./api.js");
-const homeRoutes = require("./html.js");
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
-module.export = router;
